9a911c4845f2ba61ff61873cf8942411e25679b7
Luca Baldini <luca.baldini@pi.infn.it>
2022-10-11 12:55:29 +0200

    First attempt at the group creation.

--- mkgrp.py
+++ mkgrp.py
@@ -22,9 +22,10 @@ from loguru import logger
 import pandas as pd
 
 
-FILE_PATH = 'lab1_20221009_edit.xlsx'
+FILE_PATH = 'lab1_groups_edit.xlsx'
 
 MACRO_GROUPS = ['A1', 'B1', 'A2', 'B2']
+GROUPS = [f'{grp}-{room}-{turn}' for grp in MACRO_GROUPS for room in range(1, 4) for turn in range(1, 3)]
 
 logger.info('Reading in group changes...')
 GROUP_CHANGES = {}
@@ -48,6 +49,7 @@ class Student:
     companion_name : str = None
     companion_surname : str = None
     notes : str = None
+    group : str = None
 
     def __post_init__(self) -> None:
         """Post initialization.
@@ -70,6 +72,11 @@ class Student:
         if self.companion_name is None and self.companion_surname is None:
             return None
         return f'{self.companion_name} {self.companion_surname}'
+
+    def has_companion(self) -> bool:
+        """Return True if the student has a companion.
+        """
+        return self.companion_full_name() is not None
 
 
 
@@ -118,7 +125,7 @@ class DataBase(dict):
         Fields.CompanionSurname.value : format_name,
     }
 
-    def __init__(self, file_path : str):
+    def __init__(self, file_path : str = FILE_PATH):
         """Constructor.
         """
         super().__init__(self)
@@ -134,7 +141,7 @@ class DataBase(dict):
             self[student.full_name()] = student
 
     def check_companions(self):
-        """
+        """Check the choices for the companions.
         """
         for student in self.values():
             companion_full_name = student.companion_full_name()
@@ -151,8 +158,42 @@ class DataBase(dict):
             if student.macro_group != companion.macro_group:
                 logger.error(f'Group mismatch: {student.full_name()} {student.macro_group} <-> {companion.full_name()} {companion.macro_group}')
 
+    @staticmethod
+    def dict_subset(dict_, macro_group) -> dict:
+        """
+        """
+        return {key : value for key, value in dict_.items() if key.startswith(macro_group)}
+
+    def assign_groups(self):
+        """Assign the students to the group.
+        """
+        count_dict = {grp : 0 for grp in GROUPS}
+        for student in self.values():
+            if student.group is not None:
+                continue
+            _subdict = self.dict_subset(count_dict, student.macro_group)
+            group = min(_subdict, key=_subdict.get)
+            assert group[:2] == student.macro_group
+            student.group = group
+            count_dict[group] += 1
+            if not student.has_companion():
+                continue
+            companion = self[student.companion_full_name()]
+            if companion.group is not None:
+                continue
+            logger.debug(f'Adding companion {companion.full_name()} for {student.full_name()}...')
+            companion.group = group
+            count_dict[group] += 1
+        logger.info(f'Final group numerosity: {count_dict}')
+        for macro_group in MACRO_GROUPS:
+            counts = sum(self.dict_subset(count_dict, macro_group).values())
+            logger.info(f'{macro_group} -> {counts} students')
+        return count_dict
+
+
 
 
 if __name__ == '__main__':
-    db = DataBase('lab1_20221009_edit.xlsx')
+    db = DataBase()
     db.check_companions()
+    db.assign_groups()
