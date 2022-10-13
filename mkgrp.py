c56c75777009eadfc43ed8be8eb98df3dce8536d
Luca Baldini <luca.baldini@pi.infn.it>
2022-10-13 06:44:51 +0200

    Output excel file implemented.

--- mkgrp.py
+++ mkgrp.py
@@ -25,6 +25,7 @@ import pandas as pd
 FILE_PATH = 'lab1_groups_edit.xlsx'
 
 MACRO_GROUPS = ['A1', 'B1', 'A2', 'B2']
+ROOM_GROUPS = [f'{grp}-{room}' for grp in MACRO_GROUPS for room in range(1, 4)]
 GROUPS = [f'{grp}-{room}-{turn}' for grp in MACRO_GROUPS for room in range(1, 4) for turn in range(1, 3)]
 
 logger.info('Reading in group changes...')
@@ -164,7 +165,7 @@ class DataBase(dict):
         """
         return {key : value for key, value in dict_.items() if key.startswith(macro_group)}
 
-    def assign_groups(self):
+    def assign_groups(self, file_path : str = None) -> dict:
         """Assign the students to the group.
         """
         count_dict = {grp : 0 for grp in GROUPS}
@@ -181,13 +182,32 @@ class DataBase(dict):
             companion = self[student.companion_full_name()]
             if companion.group is not None:
                 continue
-            logger.debug(f'Adding companion {companion.full_name()} for {student.full_name()}...')
+            #logger.debug(f'Adding companion {companion.full_name()} for {student.full_name()}...')
             companion.group = group
             count_dict[group] += 1
         logger.info(f'Final group numerosity: {count_dict}')
         for macro_group in MACRO_GROUPS:
             counts = sum(self.dict_subset(count_dict, macro_group).values())
             logger.info(f'{macro_group} -> {counts} students')
+        if file_path is not None:
+            logger.info(f'Writing output group file to {file_path}')
+            writer = pd.ExcelWriter(file_path, engine='xlsxwriter')
+            for group in ROOM_GROUPS:
+                logger.info(f'Writing group {group}...')
+                groups = [f'{group}-{turn}' for turn in range(1, 3)]
+                students = [student for student in self.values() if student.group in groups]
+                data = {
+                    'Nome': [student.name for student in students],
+                    'Cognome': [student.surname for student in students],
+                    'Matricola': [student.identifier for student in students],
+                    'email': [student.email for student in students],
+                    'Gruppo': [student.group for student in students]
+                }
+                df = pd.DataFrame(data)
+                df = df.sort_values(['Gruppo', 'Cognome'])
+                df.to_excel(writer, sheet_name=group)
+            writer.save()
+            logger.info('Done.')
         return count_dict
 
 
@@ -196,4 +216,4 @@ class DataBase(dict):
 if __name__ == '__main__':
     db = DataBase()
     db.check_companions()
-    db.assign_groups()
+    db.assign_groups('gruppi_lab1_2022.xlsx')
